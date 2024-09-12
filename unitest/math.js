149ca157ca5a5071622af21b471948ecc875058a
ald1ndc <aldindc@yahoo.com>
2024-09-12 20:50:24 +0800

    Unit test 2nd commit

---
+++ unitest/math.js
@@ -0,0 +1,21 @@
+function addition(a, b) {
+  return a + b;
+}
+//substract
+function subtract(a, b) {
+  return a - b;
+}
+//multiplication
+function multiply(a, b) {
+  return a * b;
+}
+//division
+function divide(a, b) {
+  return a / b;
+}
+module.exports = {
+  addition,
+  subtract,
+  multiply,
+  divide
+};
